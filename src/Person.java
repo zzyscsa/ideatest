1fb469eb3b0d686c8409a20ba053bd987ecc32a0
zzyscsa <583125807@qq.com>
2020-06-09 01:02:34 +0800

    first commit

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    public static void main(String[] args) {
+        int a = 16;
+        int b = 13;
+        System.out.println(a+b);
+        String s = "哈哈";
+        System.out.println(s);
+        if (s.charAt(0)=='a')
+            System.out.println(true);
+    }
+}
